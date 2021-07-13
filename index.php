dedbe6dee4b5b5b283b8146236d739207a8fddbb
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-07-13 18:12:23 +0200

    feat: added base path in config file

--- index.php
+++ index.php
@@ -4,7 +4,50 @@
 // the "index.php", in such a way that you must include the controller passed by the URL
 // dynamically so that it ends up including the view.
 
-include_once "config/constants.php";
-
 // TODO Implement the logic to include the controller passed by the URL dynamically
 // In the event that the controller passed by URL does not exist, you must show the error view.
+
+include_once "config/constants.php";
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "exam";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT question FROM questions WHERE questions.qst_no = 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo $row["question"];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
