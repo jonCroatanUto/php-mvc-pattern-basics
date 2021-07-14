88cf4f7a22a3d17795615c68e0568bda386439f3
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-07-14 18:47:24 +0200

    Basics style

--- views/main/main.php
+++ views/main/main.php
@@ -8,12 +8,15 @@ $id = 1;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Document</title>
 </head>
 
 <body>
-
-    <a class="start-sql-button" href="?controller=questionController&action=getQuestions&question=<?php echo $id ?>">START</a>
+    <section class="start-wrapper">
+        <h1>PHP MVC QUIZ PROJECT</h1>
+        <a class="start-sql-button" href="?controller=questionController&action=getQuestions&question=<?php echo $id ?>"><span class="start-text">START</span></a>
+    </section>
 </body>
 
 </html>
